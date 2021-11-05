4dc68957cda0324b824703dae976573079fab0ff
josiahmokob0 <jmnyarega@gmail.com>
2021-11-05 14:06:32 +0300

    - Model.findById(id, cb)

--- myApp.js
+++ myApp.js
@@ -44,7 +44,10 @@ const findOneByFood = (food, done) => {
 };
 
 const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+  Person.findById(personId, (err, data) => {
+    if (err) done(err);
+    else done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
